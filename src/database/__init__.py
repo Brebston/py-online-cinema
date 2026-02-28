4be1fa77c27b850769f18aa0888e5afdb768df80
Yurii Palahitskyi <brebston7@gmail.com>
2026-02-28 21:30:58 +0100

    fix: add __init__.py

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,8 +1,23 @@
 import os
 
 from database.models.base import Base
-
+from database.models.accounts import (
+    UserModel,
+    UserGroupModel,
+    UserGroupEnum,
+    ActivationTokenModel,
+    PasswordResetTokenModel,
+    RefreshTokenModel
+)
+from database.models.movies import (
+    MovieModel,
+    GenreModel,
+    StarModel,
+    CertificationModel,
+    DirectorModel
+)
 from database.session_sqlite import reset_sqlite_database as reset_database
+from database.validators import accounts as accounts_validators
 
 environment = os.getenv("ENVIRONMENT", "developing")
 
